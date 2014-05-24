8e3b5600135986ec91f4ebe40a6a0239e8b36f83
Danny Anderson <danny@danny-anderson.com>
2014-05-24 17:11:09 -0500

    Remove example comments from config.rb

--- config.rb
+++ config.rb
@@ -2,74 +2,17 @@
 # Blog settings
 ###
 
-# Time.zone = "UTC"
-
 activate :blog do |blog|
   blog.prefix = "blog"
-  # blog.permalink = ":year/:month/:day/:title.html"
-  # blog.sources = ":year-:month-:day-:title.html"
-  # blog.taglink = "tags/:tag.html"
-  # blog.layout = "layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = ":year.html"
-  # blog.month_link = ":year/:month.html"
-  # blog.day_link = ":year/:month/:day.html"
-  # blog.default_extension = ".markdown"
 
   blog.tag_template = "blog/tag.html"
   blog.calendar_template = "blog/calendar.html"
 
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/:num"
 end
 
 page "/feed.xml", :layout => false
 
-### 
-# Compass
-###
-
-# Susy grids in Compass
-# First: gem install susy
-# require 'susy'
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
 activate :directory_indexes
-
-# Per-page layout changes:
-# 
-# With no layout
-# page "/path/to/file.html", :layout => false
-# 
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-# 
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
 
 # Methods defined in the helpers block are available in templates
 helpers do
