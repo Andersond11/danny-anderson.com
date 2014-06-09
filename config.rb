c9853f4a771310f891092aafd7f792243e267f0c
Danny Anderson <danny@danny-anderson.com>
2014-06-08 22:34:41 -0500

    Don't build .txt files in fonts directory

--- config.rb
+++ config.rb
@@ -90,6 +90,9 @@ configure :build do
   # Or use a different image path
   # set :http_path, "/Content/images/"
 
+  # Resume specific config
+  ignore 'fonts/*.txt'
+
   # Disable this if you don't want PDF generation
   activate :pdfmaker
 end
